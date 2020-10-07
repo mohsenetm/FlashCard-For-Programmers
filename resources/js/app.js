c08089d3809e99ba582af11cea63889fe1d0b0cd
mohsenetm <56743266+mohsenetm@users.noreply.github.com>
2020-10-08 00:14:14 +0330

    edit app.js
    Edit App.js And Remove Unused Library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 require('./bootstrap');
 window.Vue=require('vue');
 window.hljs=require('highlight.js');
-import CKEditor from '@ckeditor/ckeditor5-vue';
-Vue.use( CKEditor );
-Vue.component('editor-component', require('./components/EditorComponent').default);
 window.onload = function () {
     let app = new Vue({
         el: '#app',
